4116a42193fd878391235a633ad3394c9a6f8a8f
Samuel <muka@outlook.com>
2018-10-09 22:15:22 -0300

    Commit to master

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$intervalo = 3;
+$atendimentos = array(
+    array('num_senha' => 6, 'priorit' => 8, 'dt_cheg' => '6'),
+    array('num_senha' => 1, 'priorit' => 7, 'dt_cheg' => '1')
+    ,array('num_senha' => 2, 'priorit' => 1, 'dt_cheg' => '2')
+    ,array('num_senha' => 3, 'priorit' => 1, 'dt_cheg' => '3')
+    ,array('num_senha' => 4, 'priorit' => 1, 'dt_cheg' => '4')
+    ,array('num_senha' => 5, 'priorit' => 1, 'dt_cheg' => '5')    
+    ,array('num_senha' => 7, 'priorit' => 1, 'dt_cheg' => '7')
+);
+
+foreach ($atendimentos as $key) {
+    echo $key['num_senha'].'<br>';
+}
